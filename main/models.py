9ddea23899c787bb5fda074ab7e206a420f24655
leonitacecilia <leonitacecilia19@gmail.com>
2024-09-11 13:26:31 +0700

    tutorial 2

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
+import uuid
+
 from django.db import models
 
 from django.db import models
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
@@ -11,3 +14,4 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+    
